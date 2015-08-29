8e18d15c34cc7a7ed5435eac9d3a2261d3013f21
shanky221341 <shanky221341@gmail.com>
2015-08-29 19:15:08 +0100

    refactored the code

--- xmlparse2.R
+++ xmlparse2.R
@@ -2,7 +2,7 @@ library(XML)
 #Read the file
 
 create_cell<-function(nodeName,node){
-  if(length(node)==0)
+  if(length(node)==0 )
   {
     assign(nodeName,NA,envir = .GlobalEnv)
   }else{
@@ -12,6 +12,7 @@ create_cell<-function(nodeName,node){
 }
 
 temp<-NULL
+temp1<-NULL
 
 create_observation<-function(file)
 {
@@ -22,25 +23,43 @@ xmltop<<-xmlRoot(xmlDoc)
 getNodeAndCreateCell<-function(node){
   if(node %in% names(other_tables))
   {
-    print(node)
     table_name<-node
     
     nodeAddress<-paste("//",node,sep="")
-    subnode<-getNodeSet(xmltop,nodeAddress)
+    subnode<<-getNodeSet(xmltop,nodeAddress)
     
     xmlSubNodes<-other_tables[[node]]
+    counter=0
+#     if(length(subnode)==8)
+#     {
+#       print(subnode)
+#       x<<-subnode
     
-    sub_node<<-subnode[[1]]
+    #print(length(subnode))
+    for(node in subnode){
+      # print(node)
+      counter=counter+1
+    sub_node<<-node
+    
     for (node in xmlSubNodes){
       nodeName<-node
       nodeAddress<-paste("//",node,sep="")
       node<-getNodeSet(sub_node,nodeAddress)
+      if(nodeName=='time_frame')
+      {print(node)}
+      if(nodeName!='nct_id')
       create_cell(nodeName,node)
       
     }
-    
+    if(counter==1){
+    xmlSubNodes<-append("nct_id",xmlSubNodes)
+    }
     char_vect<-sapply(xmlSubNodes,function(x)eval(parse(text=x)))
     assign(table_name,data.frame(as.list(char_vect)),envir = .GlobalEnv)
+    
+    #print(primary_outcome)
+    temp1<<-rbind(temp1,primary_outcome)
+    }
   }else{
   nodeName<-node
   nodeAddress<-paste("//",node,sep="")
