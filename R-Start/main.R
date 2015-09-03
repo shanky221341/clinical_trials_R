25a0f73b5b25f38930300d245594b715ade87d7d
Vineet <vineet@digitalhealthlabs.com>
2015-09-03 14:37:23 +0100

    made some code tidy and added the empty database "company.db" in the main project directory to be used for sqlite connection.

--- R-Start/main.R
+++ R-Start/main.R
@@ -1,70 +1,80 @@
 library(XML)
-#Read the file
+# Read the file
 
-#source all the files in R folder
-path<-list.files(pattern="[.]R$", path="../R/", full.names=TRUE)
+# source all the files in R folder
+path <- list.files(pattern = "[.]R$", path = "../R/", full.names = TRUE)
 sapply(path, source)
 
-temp<-NULL
-temp1<<-NULL
-outcome_struct<-c("measure","time_frame","safety_issue","description")
-condition_struct<-c("condition")
-arm_group_struct<-c("arm_group_label","arm_group_type","description")
-intervention_struct<-c("intervention_type","intervention_name","description","arm_group_label","other_name")
-eligibilit_struct<-c("study_pop/textblock","sampling_method","criteria/textblock","gender","minimum_age","maximum_age")
-investigator_struct<-c("first_name","last_name","middle_name","degrees","role","affiliation")
-address_struct<-c("city","state","zip","country")
-link_struct<-c("url","description")
-reference_struct<-c("citation","PMID")
-responsible_party_struct<-c("name_title","organization","responsible_party_type","investigator_affiliation","investigator_full_name","investigator_title")
+temp <- NULL
+temp1 <<- NULL
+outcome_struct <- c("measure", "time_frame", "safety_issue", "description")
+condition_struct <- c("condition")
+arm_group_struct <- c("arm_group_label", "arm_group_type", "description")
+intervention_struct <- c("intervention_type", "intervention_name", "description", "arm_group_label", "other_name")
+eligibilit_struct <- c("study_pop/textblock", "sampling_method", "criteria/textblock", "gender", "minimum_age", "maximum_age")
+investigator_struct <- c("first_name", "last_name", "middle_name", "degrees", "role", "affiliation")
+address_struct <- c("city", "state", "zip", "country")
+link_struct <- c("url", "description")
+reference_struct <- c("citation", "PMID")
+responsible_party_struct <- c("name_title", "organization", "responsible_party_type", "investigator_affiliation", "investigator_full_name", "investigator_title")
 
-#structs for results database
-group_struct<-c("title","description")
-participant_struct<-c("participant_id")
-participants_list_struct<-c("participants_list_id")
-milestone_struct<-c("title")
-measure_struct<-c("title","description","units","param","dispersion")
-results_outcome_struct<-c("type","title","description","time_frame","safety_issue","posting_date","population")
-results_outcome_struct_group<-c("NULL")
-other_tables<<-list(primary_outcome=outcome_struct,secondary_outcome=outcome_struct,other_outcome=outcome_struct,condition=condition_struct,arm_group=arm_group_struct,intervention=intervention_struct,eligibility=eligibilit_struct,overall_official=investigator_struct,address=address_struct,link=link_struct,reference=reference_struct,results_reference=reference_struct,responsible_party=responsible_party_struct,group=group_struct,participants=participant_struct,participants_list=participants_list_struct,milestone=milestone_struct,'baseline/measure_list/measure'=measure_struct,'baseline/measure_list/measure/category_list'=measure_struct,'outcome_list/outcome'=results_outcome_struct,'outcome_list/outcome/group_list'=results_outcome_struct_group)
+# structs for results database
+group_struct <- c("title", "description")
+participant_struct <- c("participant_id")
+participants_list_struct <- c("participants_list_id")
+milestone_struct <- c("title")
+measure_struct <- c("title", "description", "units", "param", "dispersion")
+results_outcome_struct <- c("type", "title", "description", "time_frame", "safety_issue", "posting_date", "population")
+results_outcome_struct_group <- c("NULL")
+other_tables <<- list(primary_outcome = outcome_struct, secondary_outcome = outcome_struct, other_outcome = outcome_struct, condition = condition_struct, arm_group = arm_group_struct, 
+    intervention = intervention_struct, eligibility = eligibilit_struct, overall_official = investigator_struct, address = address_struct, link = link_struct, reference = reference_struct, 
+    results_reference = reference_struct, responsible_party = responsible_party_struct, group = group_struct, participants = participant_struct, participants_list = participants_list_struct, 
+    milestone = milestone_struct, `baseline/measure_list/measure` = measure_struct, `baseline/measure_list/measure/category_list` = measure_struct, `outcome_list/outcome` = results_outcome_struct, 
+    `outcome_list/outcome/group_list` = results_outcome_struct_group)
 
-temporary_variables<<-paste(names(other_tables),"temp",sep="_")
-for(var in temporary_variables){assign(var,NULL)}
-assign("measure_temp",NULL)
-assign("outcome_temp",NULL)
-assign("category_details_temp",NULL)
-assign("outcome_group_temp",NULL)
-xmlNodesResults<<-c("group","participants","participants_list","milestone","measure","category_details","outcome","outcome_group")
+temporary_variables <<- paste(names(other_tables), "temp", sep = "_")
+for (var in temporary_variables) {
+    assign(var, NULL)
+}
+assign("measure_temp", NULL)
+assign("outcome_temp", NULL)
+assign("category_details_temp", NULL)
+assign("outcome_group_temp", NULL)
+xmlNodesResults <<- c("group", "participants", "participants_list", "milestone", "measure", "category_details", "outcome", "outcome_group")
 
-create_observation<-function(file)
-{
-xmlDoc<-xmlTreeParse(file)
-xmltop<<-xmlRoot(xmlDoc)
-#get the node
-
-
-xmlNodes<-c("nct_id","brief_title","acronym","official_title","source","brief_summary/textblock","detailed_description/textblock","overall_status","start_date","completion_date","primary_completion_date","phase","study_type","study_design","target_duration","number_of_arms","number_of_groups","primary_outcome","secondary_outcome","other_outcome","condition","arm_group","intervention","biospec_retention","biospec_descr/textblock","eligibility","overall_official","address","link","reference","results_reference","verification_date","lastchanged_date","firstreceived_date","firstreceived_results_date","responsible_party")
-#xmlNodes<-c("primary_outcome")
-
-sapply(xmlNodes,function(node) getNodeAndCreateCell(node))
-
-observation<<-data.frame(nct_id,brief_title,acronym,official_title,source,brief_summary,detailed_description,overall_status,start_date,completion_date,primary_completion_date,phase,study_type,study_design,target_duration,number_of_arms,number_of_groups,biospec_retention,biospec_descr,verification_date,lastchanged_date,firstreceived_date,firstreceived_results_date,stringsAsFactors = FALSE)
-temp<<-rbind(temp,observation)
+create_observation <- function(file) {
+    xmlDoc <- xmlTreeParse(file)
+    xmltop <<- xmlRoot(xmlDoc)
+    # get the node
+    
+    
+    xmlNodes <- c("nct_id", "brief_title", "acronym", "official_title", "source", "brief_summary/textblock", "detailed_description/textblock", "overall_status", "start_date", "completion_date", 
+        "primary_completion_date", "phase", "study_type", "study_design", "target_duration", "number_of_arms", "number_of_groups", "primary_outcome", "secondary_outcome", "other_outcome", 
+        "condition", "arm_group", "intervention", "biospec_retention", "biospec_descr/textblock", "eligibility", "overall_official", "address", "link", "reference", "results_reference", 
+        "verification_date", "lastchanged_date", "firstreceived_date", "firstreceived_results_date", "responsible_party")
+    # xmlNodes<-c('primary_outcome')
+    
+    sapply(xmlNodes, function(node) getNodeAndCreateCell(node))
+    
+    observation <<- data.frame(nct_id, brief_title, acronym, official_title, source, brief_summary, detailed_description, overall_status, start_date, completion_date, primary_completion_date, 
+        phase, study_type, study_design, target_duration, number_of_arms, number_of_groups, biospec_retention, biospec_descr, verification_date, lastchanged_date, firstreceived_date, 
+        firstreceived_results_date, stringsAsFactors = FALSE)
+    temp <<- rbind(temp, observation)
 }
 
-files<-dir()
+files <- dir()
 # sapply(files,function(file) create_observation(file))
 
-sapply(files,function(file) handleResultsDatabaseSeparately(file))
+sapply(files, function(file) handleResultsDatabaseSeparately(file))
 
-xml_names<-sapply(xmlNodesResults,function(node)paste(node,"temp",sep="_"))
+xml_names <- sapply(xmlNodesResults, function(node) paste(node, "temp", sep = "_"))
 
-for(i in 1:length(xmlNodesResults)){
-  assign(xmlNodesResults[i],eval(parse(text=xml_names[i])),envir = .GlobalEnv)  
+for (i in 1:length(xmlNodesResults)) {
+    assign(xmlNodesResults[i], eval(parse(text = xml_names[i])), envir = .GlobalEnv)
 }
 print("hi")
-rm(list=temporary_variables)
-observation<-temp
-results<-temp1
+rm(list = temporary_variables)
+observation <- temp
+results <- temp1
 rm(temp)
-rm(temp1)
+rm(temp1) 
--- R/handleResultsDatabaseSeparately.R
+++ R/handleResultsDatabaseSeparately.R
@@ -1,178 +1,172 @@
-handleResultsDatabaseSeparately<-function(file){
-  
-  #create the group_table
-  xmlDoc<-xmlTreeParse(file)
-  xmltop<<-xmlRoot(xmlDoc)
-  node_temp<-getNodeSet(xmltop,"//nct_id")
-  assign(xmlName(node_temp[[1]]),xmlValue(node_temp[[1]]),envir = .GlobalEnv)
-  xmlNodes<<-c("group","participants","participants_list","milestone","baseline/measure_list/measure","baseline/measure_list/measure/category_list",'outcome_list/outcome','outcome_list/outcome/group_list')
-  for(node in xmlNodes){
-  
-    if(node %in% names(other_tables))
-      {
-      if(node=='baseline/measure_list/measure')
-      {
-        table_name<-'measure'
-      }
-      else{
-        table_name<-node
-      }
-      if(table_name=="baseline/measure_list/measure/category_list"){
-        table_name<-'category_details'
-      }
-      if(table_name=='outcome_list/outcome'){
-        table_name<-'outcome'
-      }
-      if(table_name=='outcome_list/outcome/group_list'){
-        table_name<-'outcome_group'
-        node<-'outcome_list/outcome'
-      }
-      
-      # print(node)
+handleResultsDatabaseSeparately <- function(file) {
+    
+    # create the group_table
+    xmlDoc <- xmlTreeParse(file)
+    xmltop <<- xmlRoot(xmlDoc)
+    node_temp <- getNodeSet(xmltop, "//nct_id")
+    assign(xmlName(node_temp[[1]]), xmlValue(node_temp[[1]]), envir = .GlobalEnv)
+    xmlNodes <<- c("group", "participants", "participants_list", "milestone", "baseline/measure_list/measure", "baseline/measure_list/measure/category_list", 
+        "outcome_list/outcome", "outcome_list/outcome/group_list")
+    for (node in xmlNodes) {
         
-        nodeAddress<-paste("//",node,sep="")
-        subnode<<-getNodeSet(xmltop,nodeAddress)
-        
-        xmlSubNodes<-other_tables[[node]]
-        
-        counter=0
-        for(node in subnode){
-          counter=counter+1
-          sub_node<<-node
-          if(xmlName(sub_node)=="group"){
-            group_id<<-xmlGetAttr(sub_node,"group_id")
-          }
-          if(xmlName(sub_node)=="participants"){
-            group_id<<-xmlGetAttr(sub_node,"group_id")
-            count<<-xmlGetAttr(sub_node,"count")
-          }
-          if(xmlName(sub_node)=="milestone"){
-            participant_list_id<<-counter
-          }
-          if(table_name=='participants_list')
-          {
-          participant_list_id<<-counter
-          children_list<-xmlChildren(node)
-           for(child in children_list){
-             
-             group_id<<-xmlGetAttr(child,"group_id")
-             count<<-xmlGetAttr(child,"count")
-             
-             xmlSubNodes<-c("nct_id","group_id","count","participant_list_id")
-             
-             char_vect<-sapply(xmlSubNodes,function(x)eval(parse(text=x)))
-             assign(table_name,data.frame(as.list(char_vect)),envir = .GlobalEnv)
-             var_name<-paste(table_name,"temp",sep="_")
-             #print(var_name)
-             assign(var_name,rbind(eval(parse(text=var_name)),eval(parse(text=table_name))),envir = .GlobalEnv)
-           }
-          participant_list_id<<-participant_list_id+1
-          }
-          else if(table_name=='category_details'){
-            category_list_id<<-counter
-            categories<-xmlChildren(node)
-            # print(length(categories))
-            for (category in categories){
-              subtitle<-getNodeSet(category,"//sub_title")
-              # print(category)
-            
-            if(length(subtitle)!=0){
-              # print(xmlValue(subtitle[[1]]))
-              create_cell("sub_titile",subtitle)
-            }else{
-              assign("sub_title",NA,envir = .GlobalEnv)
+        if (node %in% names(other_tables)) {
+            if (node == "baseline/measure_list/measure") {
+                table_name <- "measure"
+            } else {
+                table_name <- node
+            }
+            if (table_name == "baseline/measure_list/measure/category_list") {
+                table_name <- "category_details"
+            }
+            if (table_name == "outcome_list/outcome") {
+                table_name <- "outcome"
+            }
+            if (table_name == "outcome_list/outcome/group_list") {
+                table_name <- "outcome_group"
+                node <- "outcome_list/outcome"
             }
             
-            measurements<-getNodeSet(category,"//measurement_list")
-            measurements<-measurements[[1]]
-            measurements<-xmlChildren(measurements)
-            for(measurement in measurements){
-              group_id<<-xmlGetAttr(measurement,"group_id")
-              value<<-xmlGetAttr(measurement,"value")
-              if(length(value)==0)
-                value<<-NA
-              lower_limit<<-xmlGetAttr(measurement,"lower_limit")
-              if(length(lower_limit)==0)
-                lower_limit<<-NA
-              upper_limit<<-xmlGetAttr(measurement,"upper_limit")
-              if(length(upper_limit)==0)
-                upper_limit<<-NA
-              xmlSubNodes<-c("nct_id","category_list_id","sub_title","group_id","value","lower_limit","upper_limit")
-              char_vect<-sapply(xmlSubNodes,function(x)eval(parse(text=x)))
-              assign(table_name,data.frame(as.list(char_vect)),envir = .GlobalEnv)
-              var_name<-paste(table_name,"temp",sep="_")
-              assign(var_name,rbind(eval(parse(text=var_name)),eval(parse(text=table_name))),envir = .GlobalEnv)
+            
+            
+            nodeAddress <- paste("//", node, sep = "")
+            subnode <<- getNodeSet(xmltop, nodeAddress)
+            
+            xmlSubNodes <- other_tables[[node]]
+            
+            counter = 0
+            for (node in subnode) {
+                counter = counter + 1
+                sub_node <<- node
+                if (xmlName(sub_node) == "group") {
+                  group_id <<- xmlGetAttr(sub_node, "group_id")
+                }
+                if (xmlName(sub_node) == "participants") {
+                  group_id <<- xmlGetAttr(sub_node, "group_id")
+                  count <<- xmlGetAttr(sub_node, "count")
+                }
+                if (xmlName(sub_node) == "milestone") {
+                  participant_list_id <<- counter
+                }
+                if (table_name == "participants_list") {
+                  participant_list_id <<- counter
+                  children_list <- xmlChildren(node)
+                  for (child in children_list) {
+                    
+                    group_id <<- xmlGetAttr(child, "group_id")
+                    count <<- xmlGetAttr(child, "count")
+                    
+                    xmlSubNodes <- c("nct_id", "group_id", "count", "participant_list_id")
+                    
+                    char_vect <- sapply(xmlSubNodes, function(x) eval(parse(text = x)))
+                    assign(table_name, data.frame(as.list(char_vect)), envir = .GlobalEnv)
+                    var_name <- paste(table_name, "temp", sep = "_")
+                    # print(var_name)
+                    assign(var_name, rbind(eval(parse(text = var_name)), eval(parse(text = table_name))), envir = .GlobalEnv)
+                  }
+                  participant_list_id <<- participant_list_id + 1
+                } else if (table_name == "category_details") {
+                  category_list_id <<- counter
+                  categories <- xmlChildren(node)
+                  # print(length(categories))
+                  for (category in categories) {
+                    subtitle <- getNodeSet(category, "//sub_title")
+                    # print(category)
+                    
+                    if (length(subtitle) != 0) {
+                      # print(xmlValue(subtitle[[1]]))
+                      create_cell("sub_titile", subtitle)
+                    } else {
+                      assign("sub_title", NA, envir = .GlobalEnv)
+                    }
+                    
+                    measurements <- getNodeSet(category, "//measurement_list")
+                    measurements <- measurements[[1]]
+                    measurements <- xmlChildren(measurements)
+                    for (measurement in measurements) {
+                      group_id <<- xmlGetAttr(measurement, "group_id")
+                      value <<- xmlGetAttr(measurement, "value")
+                      if (length(value) == 0) 
+                        value <<- NA
+                      lower_limit <<- xmlGetAttr(measurement, "lower_limit")
+                      if (length(lower_limit) == 0) 
+                        lower_limit <<- NA
+                      upper_limit <<- xmlGetAttr(measurement, "upper_limit")
+                      if (length(upper_limit) == 0) 
+                        upper_limit <<- NA
+                      xmlSubNodes <- c("nct_id", "category_list_id", "sub_title", "group_id", "value", "lower_limit", 
+                        "upper_limit")
+                      char_vect <- sapply(xmlSubNodes, function(x) eval(parse(text = x)))
+                      assign(table_name, data.frame(as.list(char_vect)), envir = .GlobalEnv)
+                      var_name <- paste(table_name, "temp", sep = "_")
+                      assign(var_name, rbind(eval(parse(text = var_name)), eval(parse(text = table_name))), envir = .GlobalEnv)
+                    }
+                  }
+                } else if (table_name == "outcome_group") {
+                  group_list <- getNodeSet(node, "//group_list")
+                  group_list_id <<- counter
+                  if (length(group_list) != 0) {
+                    
+                    group_list <- group_list[[1]]
+                    groups <- xmlChildren(group_list)
+                    for (group in groups) {
+                      group_id <<- xmlGetAttr(group, "group_id")
+                      node <- getNodeSet(group, "//title")
+                      create_cell("title", node)
+                      node <- getNodeSet(group, "//description")
+                      create_cell("description", node)
+                      xmlSubNodes <- c("nct_id", "group_list_id", "group_id", "title", "description")
+                      char_vect <- sapply(xmlSubNodes, function(x) eval(parse(text = x)))
+                      print(char_vect)
+                      assign(table_name, data.frame(as.list(char_vect)), envir = .GlobalEnv)
+                      var_name <- paste(table_name, "temp", sep = "_")
+                      assign(var_name, rbind(eval(parse(text = var_name)), eval(parse(text = table_name))), envir = .GlobalEnv)
+                    }
+                  }
+                } else {
+                  for (node in xmlSubNodes) {
+                    nodeName <- node
+                    nodeAddress <- paste("//", node, sep = "")
+                    node <- getNodeSet(sub_node, nodeAddress)
+                    if (!(nodeName %in% c("nct_id", "group_id", "count", "participant_list_id"))) 
+                      create_cell(nodeName, node)
+                    if (xmlName(sub_node) == "participants") {
+                      participant_id <<- counter
+                    }
+                  }
+                  if (counter == 1) {
+                    xmlSubNodes <- append("nct_id", xmlSubNodes)
+                  }
+                  if (counter == 1 & xmlName(sub_node) == "group") {
+                    xmlSubNodes <- append("group_id", xmlSubNodes)
+                  }
+                  if (counter == 1 & xmlName(sub_node) == "participants") {
+                    xmlSubNodes <- append("group_id", xmlSubNodes)
+                    xmlSubNodes <- append("count", xmlSubNodes)
+                  }
+                  if (counter == 1 & xmlName(sub_node) == "milestone") {
+                    xmlSubNodes <- append("participant_list_id", xmlSubNodes)
+                  }
+                  
+                  for (i in 1:length(xmlSubNodes)) {
+                    if (grepl("/", xmlSubNodes[i])) {
+                      xmlSubNodes[i] <- unlist(strsplit(xmlSubNodes[i], split = "/"))[1]
+                    }
+                  }
+                  
+                  char_vect <- sapply(xmlSubNodes, function(x) eval(parse(text = x)))
+                  assign(table_name, data.frame(as.list(char_vect)), envir = .GlobalEnv)
+                  
+                  # print(primary_outcome)
+                  var_name <- paste(table_name, "temp", sep = "_")
+                  # print(var_name)
+                  assign(var_name, rbind(eval(parse(text = var_name)), eval(parse(text = table_name))), envir = .GlobalEnv)
+                }
             }
-            }
-          }else if(table_name=='outcome_group'){
-            group_list<-getNodeSet(node,"//group_list")
-            group_list_id<<-counter
-            if(length(group_list)!=0)
-            {
-              
-            group_list<-group_list[[1]]
-            groups<-xmlChildren(group_list)
-            for(group in groups)
-            {
-              group_id<<-xmlGetAttr(group,"group_id")
-              node<-getNodeSet(group,"//title")
-              create_cell("title",node)
-              node<-getNodeSet(group,"//description")
-              create_cell("description",node)
-              xmlSubNodes<-c("nct_id","group_list_id","group_id","title","description")
-              char_vect<-sapply(xmlSubNodes,function(x)eval(parse(text=x)))
-              print(char_vect)
-               assign(table_name,data.frame(as.list(char_vect)),envir = .GlobalEnv)
-              var_name<-paste(table_name,"temp",sep="_")
-              assign(var_name,rbind(eval(parse(text=var_name)),eval(parse(text=table_name))),envir = .GlobalEnv)
-            }
-            }
-          }
-          else{
-          for (node in xmlSubNodes){
-            nodeName<-node
-            nodeAddress<-paste("//",node,sep="")
-            node<-getNodeSet(sub_node,nodeAddress)
-            if(!(nodeName %in% c('nct_id','group_id','count',"participant_list_id")))
-              create_cell(nodeName,node)
-            if(xmlName(sub_node)=="participants"){
-              participant_id<<-counter
-            }
-          }
-          if(counter==1){
-            xmlSubNodes<-append("nct_id",xmlSubNodes)
-          }
-          if(counter==1 & xmlName(sub_node)=="group" ){
-            xmlSubNodes<-append("group_id",xmlSubNodes)
-          }
-          if(counter==1 & xmlName(sub_node)=="participants" ){
-            xmlSubNodes<-append("group_id",xmlSubNodes)
-            xmlSubNodes<-append("count",xmlSubNodes)
-          }
-          if(counter==1 & xmlName(sub_node)=="milestone" ){
-            xmlSubNodes<-append("participant_list_id",xmlSubNodes)            
-          }
-          
-          for(i in 1:length(xmlSubNodes)){
-            if(grepl("/",xmlSubNodes[i])){
-              xmlSubNodes[i]<-unlist(strsplit(xmlSubNodes[i],split="/"))[1]
-            }
-          }
-          
-          char_vect<-sapply(xmlSubNodes,function(x)eval(parse(text=x)))
-          assign(table_name,data.frame(as.list(char_vect)),envir = .GlobalEnv)
-          
-          #print(primary_outcome)
-          var_name<-paste(table_name,"temp",sep="_")
-          #print(var_name)
-          assign(var_name,rbind(eval(parse(text=var_name)),eval(parse(text=table_name))),envir = .GlobalEnv)
+        } else {
+            nodeName <- node
+            nodeAddress <- paste("//", node, sep = "")
+            node <- getNodeSet(xmltop, nodeAddress)
+            create_cell(nodeName, node)
         }
     }
-    }else{
-      nodeName<-node
-      nodeAddress<-paste("//",node,sep="")
-      node<-getNodeSet(xmltop,nodeAddress)
-      create_cell(nodeName,node)  
-      }
-  }
-}
+} 
